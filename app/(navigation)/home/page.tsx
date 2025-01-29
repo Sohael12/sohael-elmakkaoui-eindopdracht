912340a0dcf74a29242f28678f0d882ab4696ff6
sohael12 <sohael29.2.2000@hotmail.com>
2025-01-29 14:55:21 +0100

    home page on the root

--- app/(navigation)/home/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-    return (
-        <div className="text-center text-2xl font-bold mt-20">
-            SOHAEL
-        </div>
-    );
-}
---
+++ app/(navigation)/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+      <div className="text-center text-2xl font-bold mt-20">
+        Sohael EL Makkaoui
+      </div>
+  );
+}

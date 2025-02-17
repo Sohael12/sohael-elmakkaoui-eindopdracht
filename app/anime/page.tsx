567c79f5eabc22862085c4d7872fd6dcc1e7a25d
sohael12 <sohael29.2.2000@hotmail.com>
2025-02-17 18:22:47 +0100

    update

---
+++ app/anime/page.tsx
@@ -0,0 +1,29 @@
+'use client';
+import {motion} from 'framer-motion';
+import AnimeSection from "@/app/components/Anime/animesection";
+import animes from "@/app/data";
+import Footer from "@/app/components/information/footer";
+import Navbar from "@/app/components/information/navigation";
+
+export default function seriePage() {
+    return (
+        <div className="bg-gray-900 min-h-screen text-white">
+            <Navbar/>
+
+            <motion.h1
+                initial={{opacity: 0, y: -20}}
+                animate={{opacity: 1, y: 0}}
+                transition={{duration: 0.8, ease: 'easeOut'}}
+                className="text-center text-3xl font-bold mt-20 text-yellow-400"
+            >
+                Anime Series
+            </motion.h1>
+
+            <div className="container mx-auto px-4 py-12">
+                <AnimeSection title="All Anime" animes={animes}/>
+            </div>
+
+            <Footer/>
+        </div>
+    );
+}

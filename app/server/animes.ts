839c6ca758181141b42c165681fa917a99fed5ed
sohael12 <sohael29.2.2000@hotmail.com>
2025-02-18 20:13:42 +0100

    fixed theerror on vercel

--- app/server/animes.ts
+++
@@ -1,8 +0,0 @@
-export const getAnimeById = (id) => {
-    const animes = [
-        { id: 1, title: 'Naruto', image: '/images/naruto.jpg', description: 'A ninja journey', video: 'https://www.w3schools.com/html/movie.mp4' },
-        { id: 2, title: 'One Piece', image: '/images/onepiece.jpg', description: 'Pirate adventure', video: 'https://www.w3schools.com/html/mov_bbb.mp4' },
-        // Add more anime objects here
-    ];
-    return animes.find((anime) => anime.id === parseInt(id));
-};

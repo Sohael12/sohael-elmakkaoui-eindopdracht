606d4d76ba8ce75ee8d8adf70d853713a94c28eb
sohael12 <sohael29.2.2000@hotmail.com>
2025-02-19 11:32:46 +0100

    added contact page

---
+++ app/db/schema/anime.ts
@@ -0,0 +1,11 @@
+
+export const animes = pgTable("animes", {
+    id: uuid().primaryKey().defaultRandom(),
+    title: varchar("title", { length: 255 }).notNull(),
+    image: text("image"),
+    highlightVideo: text("highlight_video"),
+    fullEpisodeVideo: text("full_episode_video"),
+    rating: numeric("rating", { precision: 3, scale: 1 }),
+    genre: text("genre"),
+    description: text("description"),
+});
